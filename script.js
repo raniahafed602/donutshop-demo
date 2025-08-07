2ff3c354bbc407d8c5c3d22f478d7fbaae1e15e1
raniahafed602 <raniahafed602@gmail.com>
2025-08-07 12:13:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+ 
+ //////////////////products
+ function changeQuantity(id, delta) {
+      
+    }
+    function addToCart(product, price, qtyInputId, image) {
+     
+      
+    }
+
+    
+  
+ let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+ 
+    function updateCart() {
+       
+    }
+
+    function removeFromCart(index) {
+     
+    }
+
+    updateCart();
+
+    function calculateTotalFromCart() {
+  
+}
+
